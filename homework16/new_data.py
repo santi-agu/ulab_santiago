431bf34e5a736e090ed96408e00f855ed25d9a9e
Santiago Agudelo Hernandez <santiago_agudelo@berkeley.edu>
2025-03-16 17:35:37 -0700

    Almost done with homework 16. Just need to do part4

---
+++ homework16/new_data.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+
+# Underlying Pattern we are trying to find 
+x_data = np.linspace(-3 * np.pi, 3 * np.pi, 1000)
+y_data = np.linspace(-3 * np.pi, 3 * np.pi, 1000)
+X, Y = np.meshgrid(x_data, y_data)
+
+pattern = np.sin(X)
+
+# Actual data that we will be using
+# Defining the dataset dimensions
+N, D_in, H, D_out = 1000, 2, 50, 1
+
+# Creating the input data
+x = torch.randn(N, D_in) * 3.1415
+y = (x[:, 0].sin()).unsqueeze(1)
+
+noise = torch.randn(N, D_out) * 0.1
+y += noise
+
+x_values = x.numpy()[:, 0]
+y_values = x.numpy()[:, 1]
+color_values = y.numpy().flatten()
